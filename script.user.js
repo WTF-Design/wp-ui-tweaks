28ce34c275415c19f8a3c263a3e3510f283f6851
Lasse Mattila <lasse.mattila@wtfdesign.fi>
2024-09-04 11:15:43 +0300

    Set icon dimensions via CSS height instead of HTML

--- script.user.js
+++ script.user.js
@@ -37,9 +37,10 @@
 		console.log("WordPress login page found.");
 		
 		const loginLink = document.createElement("a");
-		loginLink.innerHTML = `<img width="24" height="24" alt="Login" src="${GM.info.script.icon}"/>`;
+		loginLink.innerHTML = `<img alt="Login" src="${GM.info.script.icon}"/>`;
 		loginLink.href = `//${location.host}/wp-login.php`;
 		loginLink.title = "Login";
+		loginLink.style.height = "24px";
 		loginLink.style.position = "fixed";
 		loginLink.style.inset = "3px 3px auto auto";
 		loginLink.style.zIndex = "999";
